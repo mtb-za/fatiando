ddffe928ab5404d89d4b4228c539f81ad4c6de22
Martin Bentley <mtb@martinb.za.net>
2016-04-12 10:25:15 +0200

    Corrected my typo in note for upcontinue

--- fatiando/gravmag/transform.py
+++ fatiando/gravmag/transform.py
@@ -141,7 +141,7 @@ def upcontinue(x, y, data, shape, height):
 
     .. note:: x, y, z and height should be in meters.
 
-    .. note:: It is not possible to get the FFT of a masked grid. The default `grid.interp` call will not be suitable. Use `extrapolate=True` or `algorithm='nearest'`.
+    .. note:: It is not possible to get the FFT of a masked grid. The default `gridder.interp()` call using minimum curvature will not be suitable. Use `extrapolate=True` or `algorithm='nearest'`.
 
     Parameters:
 
